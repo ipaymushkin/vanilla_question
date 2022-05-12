d17653d19439e9cec58a54ba03d6ba315e78a2bf
Ilya Paymushkin <ipaymushkin76@gmail.com>
2022-05-12 12:24:09 +0300

    форма создания опросника

--- js/admin-game-create.js
+++ js/admin-game-create.js
@@ -15,4 +15,24 @@ window.addEventListener('DOMContentLoaded', () => {
         const values = getValues(form);
         console.log(values);
     });
+
+    const addQuestionButton = document.getElementById("add-question");
+    const questionsWrapper = document.getElementById("questions");
+    if (addQuestionButton) {
+        addQuestionButton.addEventListener("click", () => {
+            const id = makeId();
+            let card = '<div class="card"><div class="card-body">';
+            card += '<input name="question.name.' + id + '" class="form-control" placeholder="Вопрос" required>'
+            card += '<div class="form-group"><label>Тип вопроса<select class="form-control" name="question.type.' + id + '">'
+            card += '<option value="YESNO" selected>Да/Нет</option>'
+            card += '<option value="SINGLE">Один из списка</option>'
+            card += '<option value="MULTIPLE">Несколько из списка</option>'
+            card += '<option value="COMMENT">Комментарий</option>'
+            card += '</select></label>';
+            card += "</div></div>"
+            questionsWrapper.appendChild(createElementFromString(card));
+            const select = document.getElementsByName("question.type." + id)[0];
+            console.log(select);
+        })
+    }
 });
