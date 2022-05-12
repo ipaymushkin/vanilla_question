2458fceb1e98a8620a50bbcd10924dab51a78439
Ilya Paymushkin <ipaymushkin76@gmail.com>
2022-05-12 07:12:19 +0300

    add login forms

---
+++ js/common.js
@@ -0,0 +1,7 @@
+const alertPlaceholder = document.getElementById('alertPlaceholder');
+
+function alert(message, type) {
+    const wrapper = document.createElement('div');
+    wrapper.innerHTML = '<div class="alert alert-' + type + ' alert-dismissible" role="alert">' + message + '<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></div>';
+    alertPlaceholder.append(wrapper);
+}
