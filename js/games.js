870f3dcfe5920d4d587f0df451a23e079c427699
Ilya Paymushkin <ipaymushkin76@gmail.com>
2022-05-13 00:00:18 +0300

    fix

--- js/games.js
+++ js/games.js
@@ -2,13 +2,17 @@ window.addEventListener('DOMContentLoaded', () => {
     const tbody = document.getElementById("tbody_content");
     const games = Games.getGames();
     games.forEach((game, index) => {
-        let tr = "<tr>";
+        let tr = "<tr class='table-row-clickable' id='game-" + game.id + "'>";
         tr += "<td>" + (index + 1) + "</td>";
         tr += "<td>" + game.name + "</td>";
-        tr += "<td>" + (game.description || "").replace(/\n/g,"<br/>") + "</td>";
+        tr += "<td>" + (game.description || "").replace(/\n/g, "<br/>") + "</td>";
         tr += "<td>" + game.complexity + "</td>";
         tr += "<td>" + Object.keys(game.questions).length + "</td>";
         tr += "</tr>";
         tbody.appendChild(createElementFromString(tr));
+        const gameTr = document.getElementById("game-" + game.id);
+        gameTr.addEventListener("click", () => {
+            console.log(game.id);
+        })
     })
 });
