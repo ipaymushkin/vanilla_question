84e363f80c28f0bf8f8cf705991f945a9d20b5b6
Ilya Paymushkin <ipaymushkin76@gmail.com>
2022-05-12 11:28:34 +0300

    список пользователей

--- js/admin-game-create.js
+++ js/admin-game-create.js
@@ -1,3 +1,18 @@
 window.addEventListener('DOMContentLoaded', () => {
-    console.log('loaded');
+    const complexity = document.getElementsByName('complexity');
+    complexity.forEach(element => {
+        element.addEventListener("click", event => {
+            complexity.forEach(el => {
+                el.parentElement.classList.remove("active")
+            });
+            event.target.parentElement.classList.add("active");
+        })
+    })
+
+    const form = document.getElementById("form");
+    form.addEventListener("submit", (event) => {
+        event.preventDefault();
+        const values = getValues(form);
+        console.log(values);
+    });
 });
