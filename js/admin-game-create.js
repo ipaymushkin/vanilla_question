a61672f773d654fdc3b1a93448d83b318a04be1c
Ilya Paymushkin <ipaymushkin76@gmail.com>
2022-05-23 01:09:16 +0300

    правильные ответы

--- js/admin-game-create.js
+++ js/admin-game-create.js
@@ -49,15 +49,24 @@ window.addEventListener('DOMContentLoaded', () => {
         location.href = "/admin-game-list.html";
     });
 
+    const addYesNoBlock = (id) => {
+        let answers = '<label class="answers-container" id="yesno-answers-' + id + '">Ответы<div class="card"><div class="card-body">';
+        answers += '<div class="input-with-radio"><input required value="yes" type="radio" class="form-check-input" name="questions.' + id + '.answers" checked><input class="form-control" value="Да" disabled></div>';
+        answers += '<div class="input-with-radio"><input required value="no" type="radio" class="form-check-input" name="questions.' + id + '.answers"><input class="form-control" value="Нет" disabled></div>';
+        answers += '</div></div></label>';
+        return answers;
+    }
+
     const addQuestion = (notRemovable) => {
         const id = makeId();
         let card = '<div class="card" id="card-' + id + '"><div class="card-body">';
-        card += '<input name="question.name.' + id + '" class="form-control" placeholder="Вопрос" required>'
-        card += '<div class="form-group"><label>Тип вопроса<select class="form-control" name="question.type.' + id + '">'
-        card += '<option value="YESNO" selected>Да/Нет</option>'
-        card += '<option value="SINGLE">Один из списка</option>'
-        card += '<option value="COMMENT">Комментарий</option>'
+        card += '<input name="question.name.' + id + '" class="form-control" placeholder="Вопрос" required>';
+        card += '<div class="form-group"><label>Тип вопроса<select class="form-control" name="question.type.' + id + '">';
+        card += '<option value="YESNO" selected>Да/Нет</option>';
+        card += '<option value="SINGLE">Один из списка</option>';
+        card += '<option value="COMMENT">Комментарий</option>';
         card += '</select></label></div>';
+        card += addYesNoBlock(id);
         card += '<div class="element-wrapper" id="container-' + id + '"></div>'
         if (!notRemovable) {
             card += '<div><button type="button" class="btn btn-default" id="remove-question-' + id + '">Удалить вопрос</button></div>'
@@ -75,10 +84,14 @@ window.addEventListener('DOMContentLoaded', () => {
         const container = document.getElementById('container-' + id);
         select.addEventListener("change", (event) => {
             container.innerHTML = "";
+            const yesNoAnswers = document.getElementById("yesno-answers-" + id);
+            if (yesNoAnswers) yesNoAnswers.remove();
             if (event.target.value === "SINGLE") {
                 let answers = '<label class="answers-container">Ответы<div class="card"><div class="card-body" id="answers-' + id + '">';
-                answers += '<input name="question.answer.' + id + '.' + makeId() + '" class="form-control" placeholder="Ответ" required>'
-                answers += '<input name="question.answer.' + id + '.' + makeId() + '" class="form-control" placeholder="Ответ" required>'
+                const firstId = makeId();
+                answers += '<div class="input-with-radio"><input required value="' + firstId + '" type="radio" class="form-check-input" name="questions.' + id + '.answers" checked><input name="question.answer.' + id + '.' + firstId + '" class="form-control" placeholder="Ответ" required></div>'
+                const secondId = makeId();
+                answers += '<div class="input-with-radio"><input required value="' + secondId + '" type="radio" class="form-check-input" name="questions.' + id + '.answers"><input name="question.answer.' + id + '.' + secondId + '" class="form-control" placeholder="Ответ" required></div>'
                 answers += '</div><button type="button" class="btn btn-default" id="add-answer-' + id + '">Добавить ответ</button></div></label>';
                 container.appendChild(createElementFromString(answers));
                 const answersContainer = document.getElementById("answers-" + id);
@@ -86,7 +99,7 @@ window.addEventListener('DOMContentLoaded', () => {
                 addAnswerButton.addEventListener("click", () => {
                     const answerId = makeId();
                     let newAnswer = "<div class='answer-row' id='answer-" + answerId + "'>";
-                    newAnswer += '<input name="question.answer.' + id + '.' + answerId + '" class="form-control" placeholder="Ответ" required>'
+                    newAnswer += '<div class="input-with-radio"><input required value="' + answerId + '" type="radio" class="form-check-input" name="questions.' + id + '.answers"><input name="question.answer.' + id + '.' + answerId + '" class="form-control" placeholder="Ответ" required></div>'
                     newAnswer += '<button type="button" class="btn btn-default remove-button" id="remove-answer-' + answerId + '">Удалить ответ</button>'
                     newAnswer += "</div>";
                     answersContainer.appendChild(createElementFromString(newAnswer));
@@ -96,6 +109,14 @@ window.addEventListener('DOMContentLoaded', () => {
                         answerContainer.remove();
                     })
                 })
+            } else if (event.target.value === "YESNO") {
+                const answers = addYesNoBlock(id);
+                container.appendChild(createElementFromString(answers));
+            } else if (event.target.value === "COMMENT") {
+                let answers = '<label class="answers-container">Ответ<div class="card"><div class="card-body">';
+                answers += '<input required class="form-control" placeholder="Ответ" name="questions.' + id + '.answers">'
+                answers += '</div></div></label>';
+                container.appendChild(createElementFromString(answers));
             }
         })
     }
