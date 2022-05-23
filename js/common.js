174955e1d2d23639f237d8e1b5f427093b5f24ec
Ilya Paymushkin <ipaymushkin76@gmail.com>
2022-05-23 12:26:12 +0300

    change url

--- js/common.js
+++ js/common.js
@@ -46,7 +46,7 @@ function getRightAnswersCount(values, gameDetails) {
 }
 
 function requestGet(url) {
-    return fetch("http://localhost:5050" + url, {method: "GET"});
+    return fetch("https://vanilla-question-backend.vercel.app/" + url, {method: "GET"});
 }
 
 function requestPost(url, body = {}) {
